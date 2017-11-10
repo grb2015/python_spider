2594cec72254b717e4107f000d422ff23aba5dc6
renbin.guo <949409706@qq.com>
2017-11-10 16:26:33 +0800

    add hpu.xlsx  m excelToDatabase.py

--- injetlee_spider/excelToDatabase.py
+++ injetlee_spider/excelToDatabase.py
@@ -1,10 +1,23 @@
+'''  结果
+mysql> select *from info;
++----+---------+------+
+| id | name    | tel  |
++----+---------+------+
+|  1 | macheal | 110  |
+|  2 | Jack    | 111  |
+|  3 | Jane    | 112  |
++----+---------+------+
+3 rows in set (0.00 sec)
+'''
+
+
 from openpyxl import load_workbook
 import pymysql
 config = {
 	'host': '127.0.0.1',
 	'port':3306,
 	'user': 'root',
-	'password': 'root',
+	'password': 'password',
 	'charset': 'utf8mb4',
 	#'cursorclass': pymysql.cursors.DictCursor
 
@@ -21,9 +34,14 @@ cursor.execute('create table if not exists %s(id MEDIUMINT NOT NULL AUTO_INCREME
 wb2 = load_workbook('hpu.xlsx')
 ws=wb2.get_sheet_names()
 for row in wb2:
+	print('row = %s'%row)
 	print("1")
 	for cell in row:
-		value1=(cell[0].value,cell[4].value)
+		value1=(cell[0].value,cell[1].value)
+		print("type(value1) = %s "%type(value1))  ## tulple
+		print('cell[0] = %s'%cell[0].value)
+		print('cell[1] = %s'%cell[1].value)
+		#print('value1 = %s'%value1)
 		cursor.execute('insert into info (name,tel) values(%s,%s)',value1)
 
 print("overing...")
