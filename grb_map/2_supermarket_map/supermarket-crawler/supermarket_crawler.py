2d48e49c83dedcda98889acd549092343a3d4476
renbin.guo <949409706@qq.com>
2017-11-15 17:59:42 +0800

    add datetime to record log.txt time

--- grb_map/2_supermarket_map/supermarket-crawler/supermarket_crawler.py
+++ grb_map/2_supermarket_map/supermarket-crawler/supermarket_crawler.py
@@ -14,10 +14,12 @@ renbin.guo added:
     TODO : 
         1.将数据导入到excel中进行分析，存为csv用excel打开发现所有内容都在一个单元格
         2.麦德龙(常熟商场店) 31.684721 120.789353 经济技术开发区通港路99号  这个就不知道是哪里了，需要对地址进行结构化 /省/市/区/路/号
-        3.爬取各家超市官网，统计数据，与地图的数据做对比。
+        3.爬取各家超市官网，统计数据，与地图的数据做对比。  
+        4.要获取多个城市的多家超市，当线程太慢，需要开多任务
     history ；
         renbin.guo 2017-11-13 fix TODO1   use csv to write
         renbin.guo 2017-11-13 fix TODO2   格式化地址: 家乐福    31.241658   121.424219  白玉路101号 -->家乐福,31.241658,121.424219,上海市普陀区白玉路101号
+        renbin.guo 2017-11-15 fix TODO3   add offical_market.py 
 
 
 感谢：
@@ -30,6 +32,17 @@ import time
 import urllib.request
 
 import csv
+import datetime
+
+
+'''
+    定义log格式
+'''
+def current_time():
+     return datetime.datetime.now().strftime('%Y.%m.%d-%H:%M:%S')
+
+
+  
 
 '''
     通过经纬度得到格式化的地址
@@ -61,11 +74,11 @@ def supermarket_crawler():
     """Supermarket Crawler
     """
 
-    #cities = ['上海', '南京', '无锡', '常州', '苏州', '南通', '盐城', '扬州', '镇江', '泰州', '杭州', '宁波', '嘉兴', '湖州', '绍兴', '金华', '舟山', '台州', '合肥', '芜湖', '马鞍山', '铜陵', '安庆', '滁州', '池州', '宣城']
-    #supermarkets = ['苏果', '家乐福', '世界联华', '沃尔玛', '欧尚', '大润发', '金润发', '卜蜂莲花', '华润万家', '永辉', '金鹰', '八佰伴', '华联', '好又多', '麦德龙']
+    cities = ['上海', '南京', '无锡', '常州', '苏州', '南通', '盐城', '扬州', '镇江', '泰州', '杭州', '宁波', '嘉兴', '湖州', '绍兴', '金华', '舟山', '台州', '合肥', '芜湖', '马鞍山', '铜陵', '安庆', '滁州', '池州', '宣城']
+    supermarkets = ['苏果', '家乐福', '世界联华', '沃尔玛', '欧尚', '大润发', '金润发', '卜蜂莲花', '华润万家', '永辉', '金鹰', '八佰伴', '华联', '好又多', '麦德龙']
     #cities = ['上海', '南京', '无锡', '常州', '苏州']
-    cities = ['上海']
-    supermarkets = ['家乐福','沃尔玛','大润发',  '麦德龙']
+    #cities = ['上海']
+    #supermarkets = ['家乐福','沃尔玛','大润发',  '麦德龙']
     # test_cities = ['上海', '南京']
     # test_supermarkets = ['苏果', '家乐福']
     page_size = 20
@@ -107,7 +120,7 @@ def supermarket_crawler():
                                 print(city, supermarket_name,file=log_file)
                                 #print(city, supermarket_name)
                                 #time.sleep(1) 
-                                time.sleep(0.2)
+                                time.sleep(0.3)
                                 format_addr = get_format_addr_from_lng_lat(supermarket['location']['lat'],supermarket['location']['lng'],ak)
                                 print("_________format add = ",format_addr,file=log_file)
                                 ## windows换行需要\r\n  linux \n
@@ -116,8 +129,9 @@ def supermarket_crawler():
                                 #print('write_buf = ',write_buf,file=log_file)
                                 #print('type write_buf = ',type(write_buf,file=log_file))
                                 list_write_buf = write_buf.split(' ')
-                                print('list_write_buf = ',list_write_buf,file=log_file)
-                                print('list_write_buf = ',list_write_buf)
+                                print(current_time(),list_write_buf,file=log_file)  ### 加入时间
+                                print(current_time(),list_write_buf)
+                                #print('list_write_buf = ',list_write_buf)
                                 #market_file.write('{0}   {1}   {2} {3}\r\n'.format(supermarket['name'], supermarket['location']['lat'], supermarket['location']['lng'], supermarket['address']))
                                 
                                 writer.writerow(list_write_buf)
@@ -134,3 +148,4 @@ def supermarket_crawler():
 if(__name__ == '__main__'):
     log_file = open("./log.txt", 'a+') 
     supermarket_crawler()
+
