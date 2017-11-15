4a1dd19c17579cfd8dbe2c4bdc2cdeb37d010000
renbin.guo <949409706@qq.com>
2017-11-15 13:48:22 +0800

    m offical.py offical.csv

--- grb_map/2_supermarket_map/supermarket-crawler/offical_market_addr.py
+++ grb_map/2_supermarket_map/supermarket-crawler/offical_market_addr.py
@@ -1,13 +1,5 @@
 '''
-第一个示例：简单的网页爬虫
-
-爬取豆瓣首页
-
-
-
- renbin.guo added :
-  ###　疑问，1 .这里decode之后不就成了unicode吗?write不能写unicode啊?不然 TypeError: 'str' does not support the buffer interface
-  ###  		 2. 必须open时候必须wb,不然  error :  must be str, not bytes
+	爬取上海各大超市的地址
 '''
 
 import urllib.request
@@ -48,12 +40,12 @@ def get_metro():
   #print(data)
   #打印爬取网页的各类信息
 
-  print('type(response) = %s\n\n'%type(response))
-  print('response.geturl() = %s\n\n'%response.geturl())
-  print('response.info() = %s\n\n'%response.info())
-  print('respense.getcode() = %s\n\n'%response.getcode())
+  #print('type(response) = %s\n\n'%type(response))
+  #print('response.geturl() = %s\n\n'%response.geturl())
+  #print('response.info() = %s\n\n'%response.info())
+  #print('respense.getcode() = %s\n\n'%response.getcode())
 
-  with codecs.open('supermarkets_offical.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
+  with codecs.open('offical_markets.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
           writer = csv.writer(market_file)
           writer.writerow(["商场名","地址"])
 
@@ -67,7 +59,7 @@ def get_metro():
           for name,addr in re.findall(pattern , data):    ### 注意，这里的data必须为unicode 即str
           #for mark_list  in re.findall(pattern , data):  
              market_list=[]
-             name ='麦德龙-上海'+name+'商场'
+             name ='上海'+name+'商场'
              #print('type name ,type addr =',type(name),type(addr)) 
              #addr 为'\r\n\t\t\t\t\t\t\t\t\t\t\t上海市虹口区广粤路418号 ' 所以需要正则提取
              addr = re.findall(r'\r\n\t*(.+?)$', addr)  ## 返回的是list 
@@ -76,8 +68,9 @@ def get_metro():
              #print(addr)
              market_list.append(name)
              market_list.append(str(addr[0])) 
-             
+             market_list.insert(0,'麦德龙')
              print(market_list)
+
 
              writer.writerow(market_list)
 def get_carrefour():
@@ -88,59 +81,53 @@ def get_carrefour():
   data = response.read()                      
   data = data.decode('utf-8')     ### 同样，这里因为print(str)参数为str 所以需要unicode
 
-  print('type(response) = %s\n\n'%type(response))
-  print('response.geturl() = %s\n\n'%response.geturl())
-  print('response.info() = %s\n\n'%response.info())
-  print('respense.getcode() = %s\n\n'%response.getcode())
+  #print('type(response) = %s\n\n'%type(response))
+  #print('response.geturl() = %s\n\n'%response.geturl())
+  #print('response.info() = %s\n\n'%response.info())
+  #print('respense.getcode() = %s\n\n'%response.getcode())
 
   pattern = re.compile(r'末页.+?page=(.+?)"></a>',re.S)
   tatol_page = int(re.findall(pattern , data)[0])
-  print('tatol_page = ',tatol_page)
+  #print('tatol_page = ',tatol_page)
   for i in range(tatol_page):
     url = "http://www.carrefour.com.cn/Store/Store.aspx?&page=%s"%(i+1)
-    print("#### i =  ,url = ",i,url)
+    #print("#### i =  ,url = ",i,url)
     request = urllib.request.Request(url)
     response = urllib.request.urlopen(request)
     data = response.read()                      
     data = data.decode('utf-8')     ### 同样，这里因为print(str)参数为str 所以需要unicode
-    with codecs.open('supermarkets_offical.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
+    with codecs.open('offical_markets.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
             writer = csv.writer(market_file)
             pattern = re.compile(r'clickto.+?<a href.+?\'>(.+?)店</a></td>.+?>上海市(.+?)</a>',re.S)
-            for name,addr in re.findall(pattern , data):    ### 注意，这里的data必须为unicode 即str
-            #for car_list  in re.findall(pattern , data):   ## ('曹路', '青浦区淀山湖大道212号吾悦广场地下一层')
-
-              market_list=[]
-              name ='家乐福-上海'+name+'店'
-              addr = '上海市'+addr
-              market_list.append(name)
-              market_list.append(addr) 
-               
-              print(market_list)
-
-              writer.writerow(market_list)
-              #print('###### name = ',name)
-              #print('###### html_text = ',html_text)
-              #print('###### addr =',addr)
+            for info_tuple  in re.findall(pattern , data):  
+            	info_list = list(info_tuple)
+            	info_list[0]='上海'+info_list[0]+'店'
+            	info_list[1]='上海市'+info_list[1]
+            	info_list.insert(0,'家乐福')
+            	print(info_list)
+            	writer.writerow(info_list)
 def get_walmart():
 
   url = "http://www.wal-martchina.com/walmart/store/26_shanghai.htm"
   request = urllib.request.Request(url)
   response = urllib.request.urlopen(request)
   data = response.read()      
-  print('type(response) = %s\n\n'%type(response))
-  print('response.geturl() = %s\n\n'%response.geturl())
-  print('response.info() = %s\n\n'%response.info())
-  print('respense.getcode() = %s\n\n'%response.getcode())
+  #print('type(response) = %s\n\n'%type(response))
+  #print('response.geturl() = %s\n\n'%response.geturl())
+ # print('response.info() = %s\n\n'%response.info())
+  #print('respense.getcode() = %s\n\n'%response.getcode())
           
   #data = data.decode('utf-8',ignore)     ### 同样，这里因为print(str)参数为str 所以需要unicode
   data= data.decode('gbk')
   
-  with codecs.open('supermarkets_offical.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
+  with codecs.open('offical_markets.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
             writer = csv.writer(market_file)
             pattern = re.compile(r'170pt\'>(.+?)</td>.+?<td class=.+?>(.+?)</td>',re.S)
             for info_tuple  in re.findall(pattern , data):  
             	info_list = list(info_tuple)
-            	info_list[0]='沃尔玛-'+info_list[0]
+            	info_list.insert(0,'沃尔玛')    ## &nbsp;沃尔玛购物广场上海虹桥店  
+            	if info_list[1][0] == '&':
+            		info_list[1]=info_list[1][6:]
             	print(info_list)
             	writer.writerow(info_list)
               	
@@ -151,10 +138,10 @@ def get_rt_mark():
   request = urllib.request.Request(url)
   response = urllib.request.urlopen(request)
   data = response.read()      
-  print('type(response) = %s\n\n'%type(response))
-  print('response.geturl() = %s\n\n'%response.geturl())
-  print('response.info() = %s\n\n'%response.info())
-  print('respense.getcode() = %s\n\n'%response.getcode())
+  #print('type(response) = %s\n\n'%type(response))
+  #print('response.geturl() = %s\n\n'%response.geturl())
+  #print('response.info() = %s\n\n'%response.info())
+  #print('respense.getcode() = %s\n\n'%response.getcode())
           
   #data = data.decode('utf-8',ignore)     ### 同样，这里因为print(str)参数为str 所以需要unicode
   #data= data.decode('gbk')
@@ -169,7 +156,7 @@ def get_rt_mark():
     response = urllib.request.urlopen(request)
     data = response.read()  
     data = data.decode('utf-8') 
-    with codecs.open('supermarkets_offical.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
+    with codecs.open('offical_markets.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
               writer = csv.writer(market_file)
               pattern = re.compile(r'store_detail_head">.+?<p>(.+?)</p>.+?门店地址：(.+?)</p>',re.S)
               for info_tuple  in re.findall(pattern , data):             
@@ -179,7 +166,7 @@ def get_rt_mark():
                 print(info_list)
 
 if(__name__ == '__main__'):
-    #get_metro()
-    #get_carrefour()
-    #get_walmart()
+    get_metro()
+    get_carrefour()
+    get_walmart()
     get_rt_mark()
