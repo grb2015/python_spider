2e40195cf45f7b29db8a7db8fb4543a4c47c58b5
renbin.guo <949409706@qq.com>
2017-11-15 11:22:40 +0800

    add get_wal_market

--- grb_map/2_supermarket_map/supermarket-crawler/offical_market_addr.py
+++ grb_map/2_supermarket_map/supermarket-crawler/offical_market_addr.py
@@ -121,6 +121,55 @@ def get_carrefour():
               #print('###### name = ',name)
               #print('###### html_text = ',html_text)
               #print('###### addr =',addr)
+def get_walmart():
+
+  url = "http://www.wal-martchina.com/walmart/store/26_shanghai.htm"
+  request = urllib.request.Request(url)
+  response = urllib.request.urlopen(request)
+  data = response.read()      
+  print('type(response) = %s\n\n'%type(response))
+  print('response.geturl() = %s\n\n'%response.geturl())
+  print('response.info() = %s\n\n'%response.info())
+  print('respense.getcode() = %s\n\n'%response.getcode())
+          
+  #data = data.decode('utf-8',ignore)     ### 同样，这里因为print(str)参数为str 所以需要unicode
+  data= data.decode('gbk')
+  
+  with codecs.open('supermarkets_offical.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
+            writer = csv.writer(market_file)
+            pattern = re.compile(r'170pt\'>(.+?)</td>.+?<td class=.+?>(.+?)</td>',re.S)
+            for info_tuple  in re.findall(pattern , data):  
+            	info_list = list(info_tuple)
+            	info_list[0]='沃尔玛-'+info_list[0]
+            	print(info_list)
+            	writer.writerow(info_list)
+              	
+
+## darunfa  大润发
+def get_rt_mark():
+  url = "http://www.rt-mart.com.cn/store/detail/1001"
+  request = urllib.request.Request(url)
+  response = urllib.request.urlopen(request)
+  data = response.read()      
+  print('type(response) = %s\n\n'%type(response))
+  print('response.geturl() = %s\n\n'%response.geturl())
+  print('response.info() = %s\n\n'%response.info())
+  print('respense.getcode() = %s\n\n'%response.getcode())
+          
+  #data = data.decode('utf-8',ignore)     ### 同样，这里因为print(str)参数为str 所以需要unicode
+  data= data.decode('gbk')
+  
+  with codecs.open('supermarkets_offical.csv', 'a', encoding='utf-8') as market_file:  ### 追加写
+            writer = csv.writer(market_file)
+            pattern = re.compile(r'门店地址：(.+?)</p>',re.S)
+            for info_tuple  in re.findall(pattern , data):  
+              #writer.writerow(info_list)
+              #print('info_tuple',info_tuple)
+              info_list = list(info_tuple)
+              #print('info_list',info_list)
+
 if(__name__ == '__main__'):
-    get_metro()
-    get_carrefour()
+    #get_metro()
+    #get_carrefour()
+    get_walmart()
+    #get_rt_mark()
