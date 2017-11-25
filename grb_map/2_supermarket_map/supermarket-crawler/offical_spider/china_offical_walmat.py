296f79a2e753616fd85b2103638013ff8ecbbf2f
renbin.guo <949409706@qq.com>
2017-11-25 08:11:59 +0800

    update china_offical_walmat.py .csv

--- grb_map/2_supermarket_map/supermarket-crawler/offical_spider/china_offical_walmat.py
+++ grb_map/2_supermarket_map/supermarket-crawler/offical_spider/china_offical_walmat.py
@@ -2,7 +2,7 @@
 # @Author: Teiei
 # @Date:   2017-11-23 19:43:01
 # @Last Modified by:   Teiei
-# @Last Modified time: 2017-11-24 15:35:25
+# @Last Modified time: 2017-11-25 07:40:00
 # 
 # TODO 1
 # 1 .运行一段是就后会卡住print(city,file=log_file)打印完后
@@ -45,21 +45,23 @@
 		 </tr>
 
 '''
-#	4.一次抓取可能抓取不全，比如虽然url正确，但是有时候网络不好，请求超时，结果中应该给出哪些省份没有抓取到。
-#	5.算法改进，现在计算机要请求34*34次,显然如果前面已经找到了的城市和序号，则不需要再找了。
-#	6.安徽亳州市数据有点异常，所属城市没有抓到
+#	4.一次抓取可能抓取不全，比如虽然url正确，但是有时候网络不好，请求超时，\
+#			结果中应该给出哪些省份没有抓取到。   ## rbguo fixed 2017-11-24  use excep3_urls
+#	5.算法改进，现在计算机要请求34*34次,显然如果前面已经找到了的城市和序号，则不需要再找了。  ## rbguo fixed 2017-11-24  
+#	6.安徽亳州市数据有点异常，所属城市没有抓到    rbguo fixed 2017-11-25  use td_tags[0].get_text()  
 #	7.沃尔玛还有山姆店，但是程序没有实现抓取。
+#	8.程序结构要修改，get_single_page通过全局变量city citys 依赖到了get_walmat_urls  rbguo fixed 2017-11-25 
 #	
 #	--------结论--------------
 #	pingying.txt中要注意内蒙古，山西，陕西的写法
 #	1.全国34个省区市中，只有25个分布有沃尔玛。
-#	2.以下省份有合法的Url，但是没有分店
+#	2.以下省份有合法的Url，但是没有分店 存放在excep3_urls中：
 #	['http://www.wal-martchina.com/walmart/store/5_gansu.htm', 
 #'http://www.wal-martchina.com/walmart/store/9_hainan.htm',
 # 'http://www.wal-martchina.com/walmart/store/22_ningxia.htm',
 # 'http://www.wal-martchina.com/walmart/store/23_qinghai.htm', 
 #'http://www.wal-martchina.com/walmart/store/30_taiwan.htm']
-#   3.以下省份连合法的url都没有，当然也没有分店
+#   3.以下省份连合法的url都没有，当然也没有分店，最后剩下的citys中
 #    ['xianggang', 'aomen', 'xizang', 'xinjiang']
 
 import re
@@ -70,20 +72,16 @@ import datetime
 def current_time():   ## 记录程序运行时间
      return datetime.datetime.now().strftime('%Y.%m.%d-%H:%M:%S')
 def get_walmat_urls():
-
-	global http_times     ### 总共尝试的http请求次数
-	http_times = 0
-	global citys 
+   
+	http_times = 0 ### 总共尝试的http请求次数
 	citys = []
-	global ids
 	ids = [x for x in range(1,35)]
 	with open('pingying.txt','r+') as f:
 		for line in f.readlines():
 			citys.append(line[:-1])  ## 去掉'\n'
 	print(citys,file=log_file)
 	urls = []
-	global city
-	global id
+
 	for id in ids:
 		for city in citys:
 			print(id,file=log_file)
@@ -93,6 +91,9 @@ def get_walmat_urls():
 				http_times  = http_times+1		
 				get_single_page(url)   
 				urls.append(url)	### 如果没有出错，则说明这个url是正确的
+				citys.remove(city)	 ### rbguo fixed TODO5 2017-11-25 
+				print('#### citys = ',citys,file=log_file)
+				print('#### citys = ',citys)
 				break;
 			except:
 				print('##### except1  ',file=log_file)
@@ -102,6 +103,8 @@ def get_walmat_urls():
 		f.write(str(urls))
 	with open('excep3_urls.txt ','w+') as f:    ###将上次遍历得到的urls保存下来。
 		f.write(str(excep3_urls))	
+	print('### 共发送http url请求数：http requests  :',http_times)
+	print('### 共发送http url请求数：http requests  :',http_times,file=log_file)
 	return urls
 
 def get_single_page(url):
@@ -115,13 +118,9 @@ def get_single_page(url):
 		#print('response.geturl(,file=log_file) = %s\n\n'%response.geturl())
 		#print('response.info(,file=log_file) = %s\n\n'%response.info())
 		print('respense.getcode(,file=log_file) = %s\n\n'%response.getcode())
-		print('### city is :',city)
-		citys.remove(city)	## rbguo fix TODO5 2017-11-24  这里由于外部也正在for city in citys: 正在引用city 这样搞会不会问题？
+		## rbguo fix TODO5 2017-11-24  这里由于外部也正在for city in citys: 正在引用city 这样搞会不会问题？
 		#ids.remove(id)	## rbguo fix TODO5 2017-11-24  id不用删除，因为有break
-		print('#### citys = ',citys,file=log_file)
-		print('#### ids = ',ids,file=log_file)
-		print('#### citys = ',citys)
-		print('#### ids = ',ids)
+		
 		try:      ###如果上面都执行到了，说明url正确,但是可能解析出错，这里记录url正确，但是最终每次成功的url
 			data = response.read()
 			#data = data.decode('utf-8',ignore)     ### 同样，这里因为print(str,file=log_file)参数为str 所以需要unicode
@@ -145,9 +144,9 @@ def get_single_page(url):
 					#print('#### type(td_tags)',type(td_tags))
 					#print('#### len ',len(td_tags))
 					info_list = []
-					if(len(td_tags) == 7):
-						#print('yes == 7')
-						market_city = td_tags[0].string  ## 如果有7个td_tag则第一个就是城市名
+					if(len(td_tags) == 7): ## 如果有7个td_tag则第一个就是城市名
+						#market_city = td_tags[0].string  
+						market_city = td_tags[0].get_text()   ## fix TODO6 亳州<br>多了个<br>用string获取为none
 						market_name = td_tags[1].string
 						market_addr = td_tags[2].string
 						info_list.append("沃尔玛")
@@ -215,16 +214,14 @@ if __name__ == '__main__':
 	excep3_urls =[]  ### 记录url正确,并且请求也得到回应，但是解析出错的url (可能各个页面的结构会有不同导致)
 					 ### 另外，台湾新疆宁夏青海西藏等省没有门店，但是url也是合法的，所以会被记住
 	get_walmat()
-	
+	#url = 'http://www.wal-martchina.com/walmart/store/1_anhui.htm'
 	#url = 'http://www.wal-martchina.com/walmart/store/20_liaoning.htm'
 	#url ='http://www.wal-martchina.com/walmart/store/26_shanghai.htm'
-	#get_single_page(url)
+	#et_single_page(url)
 	end_time  = current_time()
 	print('### 程序起始时间 start at :',start_time)
 	print('### 程序结束时间 end at :',end_time)
-	print('### 共发送http url请求数：http requests  :',http_times)
-	print('### 程序起始时间 start at :',start_time，file=log_file)
-	print('### 程序结束时间 end at :',end_time，file=log_file)
-	print('### 共发送http url请求数：http requests  :',http_times，file=log_file)
+	print('### 程序起始时间 start at :',start_time,file=log_file)
+	print('### 程序结束时间 end at :',end_time,file=log_file)	
 	log_file.close()
 
