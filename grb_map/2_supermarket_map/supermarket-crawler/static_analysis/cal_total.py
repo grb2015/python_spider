f3feba56dc3c85a358ed0b498c17f213a4c69f26
renbin.guo <949409706@qq.com>
2017-12-07 08:19:41 +0800

    统计每个城市各超市的数目与总和

--- grb_map/2_supermarket_map/supermarket-crawler/static_analysis/cal_total.py
+++ grb_map/2_supermarket_map/supermarket-crawler/static_analysis/cal_total.py
@@ -5,27 +5,7 @@ def cal_total(files):
 	for file in files:
 		print('-----------file is: -----------------------------------------',file)
 		wb = load_workbook(file)
-
 		sheet = wb.get_sheet_by_name('Sheet0')  
-
-
-
-		# 获取某个单元格的值，观察excel发现也是先字母再数字的顺序，即先列再行
-		#b4 = sheet['B4']
-		# 分别返回
-		#print(f'({b4.column}, {b4.row}) is {b4.value}')  # 返回的数字就是int型
-
-		# 除了用下标的方式获得，还可以用cell函数, 换成数字，这个表示B4
-		#b4_too = sheet.cell(row=4, column=2)
-		#print(b4_too.value)
-		#b4_too = sheet.cell(4, 2)  ### 这样不行
-		#print(b4_too.value)
-		#
-		# 获取最大行的最大列
-		#print(sheet.max_row)
-		#print(sheet.max_column)
-
-
 
 		global dict_citys
 		# 因为按行，所以返回A1, B1, C1这样的顺序
@@ -43,21 +23,35 @@ def cal_total(files):
 			else:
 				dict_citys[key] =  value
 
-
 			#for cell in row:
 			#	print(cell.value)
 		print('-----------over file is: -----------------------------------------',file)
-		
+### 从超市的excel文件中获取city城市所有的超市总数，比如家乐福数，沃尔玛数，返回一个list,顺序为files的顺序
+def get_all_market_count_of_this_city(city,files):
+	counts  = []
+	for file in files:
+		print('-----------file = : -----------------------------------------',file)
+		wb = load_workbook(file)
+		sheet = wb.get_sheet_by_name('Sheet0') 
+		flag = 0
+		for row in sheet.rows:
+			if row[0].value == city:
+				flag = 1
+				print('###flag == 1,count = ',row[1].value)
 
-		# A1, A2, A3这样的顺序
-		#for column in sheet.columns:
-		  #  for cell in column:
-		   #     print(cell.value)
+				counts.append(row[1].value)
+				break;
+		if flag == 0:
+			print('#### flag = 0 ')
+			counts.append(0)  ### 如果当前file中没有当前城市，则该城市的该超市设置为0
+		print('-----------over file = : -----------------------------------------',file)
+	print('################ counts = ',counts)
+	return counts
 if __name__ == '__main__':
 	market_file =  codecs.open('china_offical_markets_total.csv', 'w+', encoding='utf-8') 
 	writer = csv.writer(market_file)
-		#writer.writerow(["城市","家乐福数","沃尔玛数","大润发数","麦德龙数"，"永辉数"，"华润万家数"])
-	writer.writerow(["城市","超市总数"])
+	writer.writerow(["城市","家乐福数","沃尔玛数","大润发数","麦德龙数","永辉数","华润万家数"])
+	#writer.writerow(["城市","超市总数"])
 
 	files = ['china_offical_carrefour_format.xlsx','china_offical_markets_walmat_format.xlsx','china_offical_markets_rt_format.xlsx'\
 		,'china_offical_metro_format.xlsx','china_offical_yh_format.xlsx' ,'china_offical_huarun_format.xlsx']
@@ -65,10 +59,17 @@ if __name__ == '__main__':
 	cal_total(files)
 		#citys = citys[1:]
 	print(dict_citys)
-	for  key in dict_citys:
+	for  key in dict_citys:     ### key代表没一个城市
 		info_list = []
 		info_list.append(key)
 		info_list.append(dict_citys[key])	
-		print(info_list)
+		print('current city is :',key)
+		
+		markets_count = []
+		markets_count = get_all_market_count_of_this_city(key,files)
+		for count in markets_count:
+			info_list.append(count)
+		print('### info_list = ',info_list)
 		writer.writerow(info_list)
+
 	market_file.close()
